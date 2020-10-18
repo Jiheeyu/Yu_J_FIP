88b5fe7c61441a6b83970ae4a0f08f982a3ca4b7
jiheeyu <j_yu@fanshaweonline.ca>
2020-10-18 17:36:21 +0900

    tryingNavbar

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@
 
 (() => {
 
+    const menuIcon = document.querySelector('.hamburger-menu');
+    const mNav = document.querySelector('.mNav');
+
+    menuIcon.addEventListener('click' , () => {
+        mNav.classList.toggle("change");
+    });
 
 })()
