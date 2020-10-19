92415432a2780e090e086f74b9635be21379f642
jiheeyu <j_yu@fanshaweonline.ca>
2020-10-20 01:25:06 +0900

    tryingAJAX

--- js/main.js
+++ js/main.js
@@ -15,14 +15,23 @@
     .then(res => res.json()) 
     .then(data => {
 
+        debugger;
+        console.log(data);
+
+        for (let proj in data) {
+            const project = document.querySelector('.proj').children;
+            project[1].textContent = data[proj].name;
+            project[2].textContent = data[proj].img;
+            project[3].textContent = data[proj].descrip;
+        };
+        // debugger;
+
+
     })
 
     .catch((err) => {
         //debugger;
         console.log(err);
-        
-        const profText = document.querySelector('.profPanelText');
-        profText.textContent = `ERROR : something went wrong`
     })
 
 })()
